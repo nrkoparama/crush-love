338f05a1a9a500f977f64781e9c80580af55e6cb
nrkoparama <139543591+nrkoparama@users.noreply.github.com>
2024-03-04 22:33:50 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Đạt có đẹp zai không!",
+  titleHeader: "Đạt có đẹp trai không?",
   descriptionHeader:
     "Hãy trả lời một cách thật lòng!",
-  buttonYes: "Yes sir, chắc chắn là như vậy rồi ",
-  buttonNo: "no no no",
-  titleModar: ":3.",
+  buttonYes: "Có",
+  buttonNo: "Không",
+  titleModar: "",
   descriptionModar:
     ":3",
 };
